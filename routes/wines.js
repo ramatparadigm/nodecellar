9978d9cd0017205f2228930e83e9a3cd9de7b73f
ramatparadigm <rambabu.k@paradigmit.com>
2014-06-26 14:25:58 +0530

    modified mongo url to heroku

--- routes/wines.js
+++ routes/wines.js
@@ -4,7 +4,7 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
+var server = new Server('ds027809.mongolab.com', 27809, {auto_reconnect: true});
 db = new Db('winedb', server, {safe: true});
 
 db.open(function(err, db) {
